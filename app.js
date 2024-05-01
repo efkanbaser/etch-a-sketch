ba68c9fb6feef460fb58383dfd7ab7cf8862f209
efkanbaser <efkanbaser26@gmail.com>
2024-05-01 15:01:43 +0300

    Added custom sized pixel option.

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
-let myContainer = document.querySelector('#container');
+let myContainer = document.querySelector('#container')
+let btn = document.getElementById('grid')
+btn.addEventListener('click', changeSize)
+addTable(16)
 
+function changeSize(){
+    newGrid = window.prompt("Choose the size of the grid between 1 and 100 pixels.")
+    while(newGrid > 101 || newGrid < 1){
+        newGrid = window.prompt("Choose the size of the grid between 1 and 100 pixels.") 
+    }
+    removeTable()
+    addTable(newGrid)
+}
 
-function addSquare(){
-    for(let i=0; i<256; i++) {
-        let gridSquare = document.createElement('div')
-        gridSquare.addEventListener("mouseenter", () => gridSquare.setAttribute("style", "background-color:blue;"))
-        //gridSquare.addEventListener("mouseleave", () => gridSquare.setAttribute("style", "background-color:white;"))
-        myContainer.appendChild(gridSquare)
-        gridSquare.className = "square"
+function addTable(size){
+    for(let i=0; i<size; i++){
+        let gridRow = document.createElement('div')
+        myContainer.appendChild(gridRow)
+        gridRow.className = "row"
+        for(let j=0; j<size; j++) {
+            let gridSquare = document.createElement('div')
+            gridSquare.addEventListener("mouseover", () => gridSquare.setAttribute("style", "background-color:blue;"))
+            //gridSquare.addEventListener("mouseleave", () => gridSquare.setAttribute("style", "background-color:white;"))
+            gridRow.appendChild(gridSquare)
+            gridSquare.className = "square"
+            }
     }
 }
-addSquare()
+
+function removeTable(){
+    myContainer.innerHTML = ""
+}
+
+
